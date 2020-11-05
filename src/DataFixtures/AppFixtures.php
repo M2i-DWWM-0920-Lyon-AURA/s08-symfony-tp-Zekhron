b73085c43ef4b7667d3bc89eb6e3d45d380c9d9a
[Zekhron] <[guillaume.bertomeu69@gmail.com]>
2020-11-05 14:51:27 +0100

    Add AppFixtures /!\

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,6 +12,9 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $data = file_get_contents('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772');
+        $json_data = json_decode($data, true);
+
         $manager->flush();
     }
 }
