5376e434f4080d0b378e911eb48bffcd5beb6321
[Zekhron] <[guillaume.bertomeu69@gmail.com]>
2020-11-05 14:21:13 +0100

    Add Entity class

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IngredientRepository::class)
+ */
+class Ingredient
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $measure;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMeasure(): ?string
+    {
+        return $this->measure;
+    }
+
+    public function setMeasure(string $measure): self
+    {
+        $this->measure = $measure;
+
+        return $this;
+    }
+}
